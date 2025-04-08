b76a8ac9265638893306e46ced07791d5dbe842c
atuli-gorgodze <atuligorgodze27@gmail.com>
2025-04-08 17:54:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -44,12 +44,6 @@ app.get("/work", function(req,res){
     res.render("list", {listTitle:"Work List", newListItems: workItems})
 });
 
-// app.post("/work", function(req,res){
-//     let item = req.body.newItem;
-//     workItems.push(item);
-//     res.redirect("/work");
-// })
-
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
